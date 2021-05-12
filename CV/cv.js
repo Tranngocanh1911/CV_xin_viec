44359b184ef305e09afd72d7d4e920a48eb4a021
Tranngocanh1911 <83797409+Tranngocanh1911@users.noreply.github.com>
2021-05-12 08:41:30 +0700

    ban cv moi

---
+++ CV/cv.js
@@ -0,0 +1,50 @@
+function hocvann() {
+    document.getElementById('thpt').innerHTML = "<strong>THPT Trần Phú-Hoàn Kiếm<strong></strong>"
+}
+
+function doilai() {
+    document.getElementById('thpt').innerHTML = "Học Vấn: cấp 3"
+}
+
+function doianh() {
+    document.getElementById('jennie').src = "jisoo.jpg";
+}
+
+function jenniequaylai() {
+    document.getElementById('jennie').src = "jennie.jpg";
+}
+
+function doiten(){
+    document.getElementById('ten').innerHTML ="<strong>Họ và tên: Jenanh</strong>"
+}
+
+function tenthat(){
+    document.getElementById('ten').innerText ="Họ và tên: Trần Ngọc Anh"
+}
+
+function doidiachi(){
+    document.getElementById('dia chi').innerHTML ="<strong>Địa chỉ: số 2 Hai Bà Trưng</strong"
+}
+
+function diachicu(){
+    document.getElementById('dia chi').innerText ="Địa chỉ: số 1 Hai Bà Trưng"
+}
+function doisdt(){
+    document.getElementById('sdt').innerHTML ="<strong>Số điện thoại: 0987654321</strong"
+}
+function sdtcu(){
+    document.getElementById('sdt').innerText ="Số điện thoại: 0123456789"
+}
+
+function  emailphu(){
+    document.getElementById('email').innerHTML ="<strong>Email: tranngocanh19@gmail.com</strong>"
+}
+function emailchinh(){
+    document.getElementById('email').innerText ="Email: tranngocanh@gmail.com"
+}
+function kynangthat(){
+    document.getElementById('kynang').innerHTML ="<strong>Kỹ năng: ngủ</strong>"
+}
+function kynanggia(){
+    document.getElementById('kynang').innerText ="Kỹ năng: lập trình viên Fullstack"
+}
